d8b1a033a4b62ba5b1a947b73d5e5423bfef4d3e
Mitchell Goodwin <mgoodwin777@gmail.com>
2020-04-26 15:04:16 -0400

    Finished part 16

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
-const getNotes = require('./notes')
 const chalk = require('chalk')
+const yargs = require('yargs')
 
-const command = process.argv[2]
+const getNotes = require('./notes')
 
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-}
+// Customize yargs version
+yargs.version('1.1.0')
 
-console.log(process.argv)
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note!')
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'Listing the notes',
+    handler: function () {
+        console.log('Listing all the notes')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Reads a note',
+    handler: function () {
+        console.log('Reads a note')
+    }
+})
+
+// add, remove, read, list
+
+console.log(yargs.argv)
